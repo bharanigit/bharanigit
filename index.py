fba00c6b527defe643e1ba5a42c2ae00ac121f3b
bharani <b.a.muthukumar@avanade.com>
2022-11-09 21:41:55 +0100

    copilot helloworld  🏆🏆

---
+++ index.py
@@ -0,0 +1,54 @@
+# find the smallest elements in a list wihtout using min() function and sort() function
+
+# find the smallest element in a list
+
+
+
+# find the smallest element in a list without using min() function and sort() function
+
+""" find factorial of a number """      # find the factorial of a number        
+
+import pandas as pd
+
+class Number:
+    x = 0
+    
+    func = lambda x: x * (x-1)
+    
+    def Number(self, x):
+        self.x = x
+    
+    def Sum(self):
+        return self.x + self.x
+    
+    # find prime numbers in a list
+    
+    # how to find the smallest element in a list without using min() function and sort() function
+    # find the smallest element in a list without using min() function and sort() function      
+    
+    def addTwoNumbers(self, l1, l2):
+        # add two numbers
+        return l1 + l2
+    
+    def ReadCsv(self, file):
+        # read csv file
+        return pd.read_csv(file)
+    
+    def ReplaceSpecialCharacters(self, string):
+        # replace special characters
+        return string.replace(' ', '')
+    
+    def SortArray(self, array):
+        # sort array
+        return sorted(array)
+    
+    def ReadCsvIgnoreCommant(self, file):
+        # read csv file and ignore comments
+        return pd.read_csv(file, comment='#')
+    def ReadCsvIgnorenrows(self, file):
+        # read csv file and ignore comments
+        return pd.read_csv(file, nrows=2)
+    
+    def ReadCsvIgnoreSpecialCharacters(self, file):
+        # read csv file and ignore special characters
+        return pd.read_csv(file, sep=';')
